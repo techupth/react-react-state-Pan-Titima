e7eb77652b66c5b36034ed716d59652d8e031072
Titima Kasemsritanawat <titima.kp@gmail.com>
2024-08-13 01:00:33 +0700

    Answer Exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+
+const[message, setMessage] = useState("Greeting Message");
+const thaiMessage = () => {
+  setMessage("สวัสดี!");
+};
+const englishMessage = () => {
+  setMessage("Hi!");
+};
+const chineseMessage = () => {
+  setMessage("你好!");
+};
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{message}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={thaiMessage}>สวัสดี!</button>
+        <button onClick={englishMessage}>Hi!</button>
+        <button onClick={chineseMessage}>你好!</button>
       </div>
     </div>
   );
